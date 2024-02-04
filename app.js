6c0fff8cfc50b1459c7692c547833bc71d460940
alanjoshua05 <155869499+alanjoshua05@users.noreply.github.com>
2024-02-04 16:26:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+function mat() {
+    var childPageContainer = document.getElementById("matc");
+    // Toggle visibility of the child page container
+    childPageContainer.style.display =
+        childPageContainer.style.display === "none" ? "block" : "none";
+}
+function phy() {
+    var childPageContainer = document.getElementById("phyc");
+    // Toggle visibility of the child page container
+    childPageContainer.style.display =
+        childPageContainer.style.display === "none" ? "block" : "none";
+}
+function chem() {
+    var childPageContainer = document.getElementById("chemc");
+    // Toggle visibility of the child page container
+    childPageContainer.style.display =
+        childPageContainer.style.display === "none" ? "block" : "none";
+}
+function eng() {
+    var childPageContainer = document.getElementById("engc");
+    // Toggle visibility of the child page container
+    childPageContainer.style.display =
+        childPageContainer.style.display === "none" ? "block" : "none";
+}
+function c() {
+    var childPageContainer = document.getElementById("cc");
+    // Toggle visibility of the child page container
+    childPageContainer.style.display =
+        childPageContainer.style.display === "none" ? "block" : "none";
+}
+function beee() {
+    var childPageContainer = document.getElementById("beec");
+    // Toggle visibility of the child page container
+    childPageContainer.style.display =
+        childPageContainer.style.display === "none" ? "block" : "none";
+}
